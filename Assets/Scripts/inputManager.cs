2836b8f107c8d000b2ec51b3224e4276cb534769
Manouj Kumar <mxg1610@miami.edu>
2019-10-29 00:24:51 -0400

    InitUpload

---
+++ Assets/Scripts/inputManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inputManager : MonoBehaviour
+{
+    public GameObject cubeObject;
+    public Transform selectedObject;
+    public VrGrabber.VrgGrabber grabber;
+    // Start is called before the first frame update
+    void Start()
+    {
+        selectedObject = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (VrGrabber.VrgGrabber.hitObj.GetComponent<SphereChanger>()!=null)
+        {
+            
+            if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) || Input.GetKeyDown(KeyCode.C))
+            {
+                selectedObject = VrGrabber.VrgGrabber.hitObj.GetComponent<SphereChanger>().nextSphereObject;
+                VrGrabber.VrgGrabber.hitObj.GetComponent<SphereChanger>().ChangeSphere(selectedObject);
+            }
+        }
+
+
+        
+        
+        
+    }
+}
